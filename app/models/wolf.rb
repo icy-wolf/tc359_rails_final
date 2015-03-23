2520686bad2f55528251e50fd73abe848034d711
icy-wolf <nicole.lawrence93@gmail.com>
2015-03-23 12:27:00 -0400

    Fixed validation issue

--- app/models/wolf.rb
+++ app/models/wolf.rb
@@ -1,7 +1,7 @@
 class Wolf < ActiveRecord::Base
   validates_presence_of :name, :species, :pack_rank
   validates_presence_of :pack_number, :message =>"Pack number required if not loner", :if => :not_loner?
-  validates :pack_number, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :pack_number, :numericality => { :greater_than_or_equal_to => 0 }, :if => :not_loner?
 
   def not_loner?
     pack_rank != 'Loner'
