680b30677af8c9a8dc48c32299579855d1bb9860
icy-wolf <nicole.lawrence93@gmail.com>
2015-03-30 04:10:39 -0400

    Fixed the repeating pack number deal, just can't figure out the label of Isalive

--- app/models/wolf.rb
+++ app/models/wolf.rb
@@ -1,6 +1,6 @@
 class Wolf < ActiveRecord::Base
   validates_presence_of :name, :species, :pack_rank
-  validates_presence_of :pack_number, :message =>"Pack number required if not loner", :if => :not_loner?
+  validates_presence_of :pack_number, :message =>"required if not loner", :if => :not_loner?
   validates :pack_number, :numericality => { :greater_than_or_equal_to => 0 }, :if => :not_loner?
 
   def not_loner?
