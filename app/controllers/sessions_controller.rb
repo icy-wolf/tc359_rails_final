5e6eca3927f1bd6e1427ea6936e2a4280d5e8695
icy-wolf <nicole.lawrence93@gmail.com>
2015-04-28 10:43:27 -0400

    Simple form completion

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,30 +4,24 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
+    user = User.find_by(email: user_params[:email])
+    if user && user.authenticate(user_params[:password])
       session[:user_id] = user.id
       redirect_to root_url, notice: 'Logged in!'
     else
       flash.alert = 'Invalid email/password combination. Please try again.'
+      @user = User.new
       render :new
     end
   end
 
-  def register
-    user = User.find_by(email: params[:email])
-    if user
-      flash.alert = "That email is already in use. Please try again."
-    elsif :password != :confirm_password
-      flash.alert = "The passwords do not match. Please try again."
-    else
-      #add to table
-      redirect_to root_url, notice: 'Logged in!'
-    end
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: 'Successfully signed out!'
   end
 
-  def destroy
-  session[:user_id] = nil
-  redirect_to root_url, notice: 'Successfully signed out!'
-  end
+  private
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
 end
